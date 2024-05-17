f30909c8d2b9ab476814dc7f6fd57640ce828af6
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-05-17 14:03:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Informações fixas
-    const birthDate = new Date(1978, 5, 25); // Ano, Mês (0-11), Dia
+    const birthDate = new Date(1978, 4, 25); // Ano, Mês (0-11), Dia (Maio é 4, pois é indexado a partir de 0)
 
     // Elementos do DOM
-    document.getElementById("birthdate").textContent = birthDate.toLocaleDateString("pt-BR");
+    document.getElementById("birthdate").textContent = birthDate.toLocaleDateString("pt-BR", { day: 'numeric', month: 'long' });
 
     // Data atual
     const currentDate = new Date();
@@ -24,6 +24,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Calcular a diferença de dias
     const oneDay = 24 * 60 * 60 * 1000; // Horas * Minutos * Segundos * Milissegundos
-    const daysUntilBirthday = Math.round((nextBirthday - currentDate) / oneDay);
+    const daysUntilBirthday = Math.ceil((nextBirthday - currentDate) / oneDay);
     document.getElementById("days-until-birthday").textContent = daysUntilBirthday + " dias";
 });
